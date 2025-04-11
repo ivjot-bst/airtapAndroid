ab470385364277328d42bac284453a1e278e6444
ivjot-bst <ivjot.singh@bluestacks.com>
2025-04-11 14:15:00 +0530

    Add stream module structure

---
+++ stream/index.js
@@ -0,0 +1,10 @@
+/**
+ * Stream Module for AirTap Android SDK
+ * Exports the stream manager for WebRTC streaming
+ */
+
+const StreamManager = require('./manager');
+
+module.exports = {
+  StreamManager
+};
