250a6f954e9bb2981a39418990bd533b45483862
ivjot-bst <ivjot.singh@bluestacks.com>
2025-04-14 16:20:00 +0530

    Add app management module structure

---
+++ apps/index.js
@@ -0,0 +1,10 @@
+/**
+ * App Management Module for AirTap Android SDK
+ * Exports the app manager for installing, launching, and managing apps
+ */
+
+const AppManager = require('./manager');
+
+module.exports = {
+  AppManager
+};
