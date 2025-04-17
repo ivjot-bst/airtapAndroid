694b681b8a1e5b8ab84d748e0811aff9b8d50f57
ivjot-bst <ivjot.singh@bluestacks.com>
2025-04-17 15:45:00 +0530

    Add recording module structure

---
+++ record/index.js
@@ -0,0 +1,10 @@
+/**
+ * Recording Module for AirTap Android SDK
+ * Exports the record manager for screen recording
+ */
+
+const RecordManager = require('./manager');
+
+module.exports = {
+  RecordManager
+};
