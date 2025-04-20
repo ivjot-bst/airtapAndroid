87ef77731f0bee73bda613e4d86003ffd938a736
ivjot-bst <ivjot.singh@bluestacks.com>
2025-04-20 13:40:00 +0530

    Add file management module structure

---
+++ files/index.js
@@ -0,0 +1,10 @@
+/**
+ * File Management Module for AirTap Android SDK
+ * Exports the file manager for file operations
+ */
+
+const FileManager = require('./manager');
+
+module.exports = {
+  FileManager
+};
