e7c3ce8cb83929094fb18b05a8ef78e8be9eca1f
ivjot-bst <ivjot.singh@bluestacks.com>
2025-04-23 14:50:00 +0530

    Add API client for service communication

---
+++ api/index.js
@@ -0,0 +1,10 @@
+/**
+ * API Client Module for AirTap Android SDK
+ * Exports the client for making requests to the AirTap service
+ */
+
+const ApiClient = require('./client');
+
+module.exports = {
+  ApiClient
+};
